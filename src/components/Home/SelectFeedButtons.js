61585768a91eddf8b35e3196bc85619ae94bcb84
moshe <45424025+mmoshem@users.noreply.github.com>
2025-07-15 14:21:58 +0300

    update All Feed button in the back side also

--- src/components/Home/SelectFeedButtons.js
+++ src/components/Home/SelectFeedButtons.js
@@ -6,7 +6,7 @@ export default function SelectFeedButtons({setFilterBy,onSelect}){
 
     return (
         <div className={styles.selectFeedContainer}>
-            <button className={styles.selectFeedButton} onClick={()=>{setFilterBy('none');onSelect()}}>no filter in feed</button>
+            <button className={styles.selectFeedButton} onClick={()=>{setFilterBy('none');onSelect()}}>All Feed</button>
             <button className={styles.selectFeedButton} onClick={()=>{setFilterBy('onlyGroupsIFollow');onSelect()}}>only from groups i follow </button>
             <button className={styles.selectFeedButton} onClick={()=>{setFilterBy('none');onSelect()}}>none</button>
             <button className={styles.selectFeedButton} onClick={()=>{setFilterBy('none');onSelect()}}>none</button>
